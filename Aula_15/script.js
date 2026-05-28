45e9208f4cdfbb9af7d57a7a5c71d023ca59d845
pedro_caprini <pedrocapriniv@gmail.com>
2026-05-27 22:25:52 -0300

    .

--- Aula_15/script.js
+++ Aula_15/script.js
@@ -1,14 +1,29 @@
 function calcular() {
-    
     let ano = document.getElementById("ano").value;
     let resultado = document.getElementById("resultado");
+
+    if (ano == "") {
+        alert("Digite um ano válido");
+        return;
+    }
+
     let dataAtual = new Date();
     let anoAtual = dataAtual.getFullYear();
     let idade = anoAtual - parseInt(ano);
 
-    if (ano == 0){
-        alert("Digite um ano válido")
-        return
+    resultado.innerHTML = "Sua idade é: " + idade + " anos";
+}
+
+function idadeTudo() {
+    let dia = document.getElementById("dia").value;
+    let mes = document.getElementById("mes").value;
+    let ano = document.getElementById("anoNascimento").value;
+    let resultado2 = document.getElementById("resultado2");
+
+    if (dia == "" || mes == "" || ano == "") {
+        alert("Preencha dia, mês e ano.");
+        return;
     }
-    resultado.innerHTML = "Sua idade é: " + idade;
+
+    resultado2.innerHTML = "Sua idade é " + dia + " dias, " + mes + " meses e " + ano + " anos";
 }
